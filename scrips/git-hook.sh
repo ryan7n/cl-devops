a886b9c88333aa06da5e0a729b372d29523d11dd
ray <ray@com>
2020-07-11 17:48:23 +0800

    add hook

--- scrips/git-hook.sh
+++ scrips/git-hook.sh
@@ -24,7 +24,6 @@ echo "deploying the web project"
 git fetch --all  #这里使用git fetch进行拉取，不建议用git pull
 git reset --hard origin/master
 
-
 time=`date`
 
 echo "web server pull at webserver at time: $time."
