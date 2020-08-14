c9d844939ba1901d52404410278e94d0bdedd10e
ray <ray@com>
2020-08-15 02:11:58 +0800

    add nginx conf

--- scrips/cron.sh
+++ scrips/cron.sh
@@ -3,7 +3,7 @@
 crontab -l >/tmp/mycron
 basepath=$(cd `dirname $0`; pwd)
 
-echo "* * * * * flock /tmp/rn $basepath/reload-nginx.sh $basepath/../nginx/conf.d" >>/tmp/mycron
+echo "* * * * * flock -n /tmp/rn $basepath/reload-nginx.sh $basepath/../nginx/conf.d" >>/tmp/mycron
 crontab /tmp/mycron
 
 rm /tmp/mycron
