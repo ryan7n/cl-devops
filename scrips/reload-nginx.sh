f1d5cf216816916de8c07071ba6d22309112bd8b
ray <ray@com>
2020-08-15 02:26:30 +0800

    add nginx conf

--- scrips/reload-nginx.sh
+++ scrips/reload-nginx.sh
@@ -8,6 +8,6 @@ script=1
 
 inotifywait -mrq --format '%e' --event create,delete,modify  $filename | while read event
   do
-      case $event in MODIFY|CREATE|DELETE) (echo $event;cd ../devops &&  echo $PWD && /usr/local/bin/docker-compose -f docker-compose.yml restart webserver)  ;;
+      case $event in MODIFY|CREATE|DELETE) (echo $event;cd `dirname $0`/../ &&  echo $PWD && /usr/local/bin/docker-compose -f docker-compose.yml restart webserver)  ;;
       esac
   done
